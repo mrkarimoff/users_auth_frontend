3f96ad1e82ff6a3d6d24e67866fca5b759aad938
mrkarimoff <mrkarimoff99@gmail.com>
2023-01-25 13:58:39 +0500

    changed to hasRouter

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, createHashRouter } from "react-router-dom";
 import Cabinet from "./routes/Cabinet";
 import ErrorPage from "./routes/ErrorPage";
 import SignIn from "./routes/SignIn";
 import SignUp from "./routes/SignUp";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/users_auth_frontend",
     element: <Cabinet />,
